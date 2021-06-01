66965d0495a1beb261d91547487c7bf6d2a80e36
Minjeong Jang <minjeongg24@gmail.com>
2021-06-01 16:01:13 +0100

    added index for furnishing

--- app/controllers/furnishings_controller.rb
+++ app/controllers/furnishings_controller.rb
@@ -1,2 +1,48 @@
 class FurnishingsController < ApplicationController
+  def index
+    @furnishings = Furnishing.all
+  end
+
+  def show
+    @furnishing = Furnishing.find(params[:id])
+  end
+
+  def new
+    @furnishing = Furnishing.new
+  end
+
+  def create
+    @furnishing = Furnishing.new(strong_params)
+    @furnishing.user_id = current_user.id
+    if @furnishing.save
+      redirect_to furnishing_path(@furnishing)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @furnishing = Furnishing.find(params[:id])
+  end
+
+  def update
+    @furnishing = Furnishing.find(params[:id])
+    if @furnishing.update(strong_params)
+      redirect_to furnishing_path(@furnishing)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @furnishing = Furnishing.find(params[:id])
+    @furnishing.destroy
+    redirect_to furnishings_path
+  end
+
+  private
+
+  def strong_params
+    params.require(:furnishing).permit(:title, :description, :price, :location, :furnishing_type)
+  end
 end
