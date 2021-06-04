0f75b20ec943b43facb27699ef0514ee5a028da2
Heba Malik <hebazmalik@gmail.com>
2021-06-04 15:07:39 +0100

    added seeds to timmy

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,3 +68,17 @@ user2 = User.create!(email: "timmy@gmail.com", password: "123456")
 
 puts "Created a user with email: timmy@gmail.com and password: 123456"
 
+furnishing_attributes = [
+  { title: "Mid-Century Floor Lamp", description: "Beautiful Lamp from the 1960s", price: 100, location: "London", furnishing_type: "Lamp" },
+  { title: "Parker Knoll Sofa", description: "Vintage Green 2-seater Sofa", price: 320, location: "Paris", furnishing_type: "Sofa"},
+  { title: "Persian Rug", description: "Vintage handmade, red rug", price: 400, location: "London", furnishing_type: "Rug"}, { title: "Danish Glass Vases", description: "3 Antique Green Vases", price: 50, location: "Germany", furnishing_type: "Vase"}
+]
+
+furnishing_attributes.each do |furnishing_attribute|
+  user_attribute = {user_id: user2.id}
+  furnishing = Furnishing.new(furnishing_attribute.merge(user_attribute))
+
+  file = URI.open("https://source.unsplash.com/featured/?furniture")
+  furnishing.photo.attach(io: file, filename: "chairs", content_type: file.content_type_parse.first)
+  furnishing.save!
+end
